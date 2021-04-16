2b775337380670a553418cc3d5a3d1bc4f562af8
Olivier Condat <ocondat@gmail.com>
2021-04-16 15:21:17 +0200

    First function dotnetcore_httptrigger

---
+++ README.md
@@ -0,0 +1,5 @@
+# First Azure Function in donet core 5.0 isolated
+
+- project create from visual code with Azure Functions plugin
+- Function deployed using the same plugin:
+    - very minimalist function => dotnetcore_httptrigger
---
+++ dotnetcore-httptrigger.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text;
+
+namespace mytutorial.Function
+{
+    public static class dotnetcore_httptrigger
+    {
+        [Function("dotnetcore_httptrigger")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("dotnetcore_httptrigger");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            if(req.Method == "GET"){
+                response.WriteString("Welcome to Azure Functions!");
+            }
+            else if(req.Method == "POST"){
+                response.WriteString("POST detected: \n");
+                response.WriteString("url: " + req.Url + "\n");
+                response.WriteString("Body: \n" );
+
+                using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+                {  
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        response.WriteString(line);
+                    }
+                }
+            }
+            else{
+                response.WriteString("unsupported method: " + req.Method);
+            }
+
+            return response;
+        }
+    }
+}
