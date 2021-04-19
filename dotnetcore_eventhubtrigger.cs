7b7499e72a37be9685044c7a385376b1d708f64b
Olivier Condat <ocondat@gmail.com>
2021-04-19 16:05:54 +0200

    extract temperature & humidity from iot message

--- dotnetcore_eventhubtrigger.cs
+++ dotnetcore_eventhubtrigger.cs
@@ -1,16 +1,33 @@
 using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace dotnetfirstapi.Function
 {
+    public partial class Sensordata
+    {
+        /// <summary> Initializes a new instance of Sensordata. </summary>
+        /// <param name="temperature"> . </param>
+        /// <param name="humidity"> . </param>
+        public float temperature { get; set;}
+        public float humidity { get; set;}
+    }
+
     public static class dotnetcore_eventhubtrigger
     {
         [Function("dotnetcore_eventhubtrigger")]
         public static void Run([EventHubTrigger("samples-workitems", Connection = "AzureEventHubConnectionString")] string[] input, FunctionContext context)
         {
             var logger = context.GetLogger("dotnetcore_eventhubtrigger");
-            logger.LogInformation($"First Event Hubs triggered message: {input[0]}");
+
+            string intermediate = $"{input[0]}";
+            logger.LogInformation($"print debug intermediate version 3: {intermediate}");
+
+            Sensordata data = JsonSerializer.Deserialize<Sensordata>(intermediate);
+            
+            logger.LogInformation($"find temperature: {data.temperature}");
+            logger.LogInformation($"find Humidity: {data.humidity}");
         }
     }
 }
